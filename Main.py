fbf56d20a27ce8cdfbd677fd19539ee05c1a1d2c
Matthew <matthewmz@me.com>
2019-09-14 21:31:05 -0400

    fixed

--- Main.py
+++ Main.py
@@ -1,5 +1,12 @@
 # Matthew Zung
-# Physics knowledge game
+# Multiple Games!
 
-print("Welcome")
+print("Welcome to my Game show!")
+print("Please type out the trivia question subject you would like to play from this list.")
+print("Math, Physics, Computer Science, Law, History, and Geography.")
 
+
+print("Since I don't know how to select from a list yet, we will do math.")
+
+answer = input("What is 13 * 13?")
+print("Yes it is 169.")
